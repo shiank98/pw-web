8562effc9c9755f4ea38f13a25a1e5bc59bdd5d3
shiank98 <wesmontpaglomutan@gmail.com>
2019-10-31 13:37:27 +0800

    header.blade.php
    added a hide function to the buy button to prevent spamming.

--- resources/views/front/header.blade.php
+++ resources/views/front/header.blade.php
@@ -1,174 +1,113 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="utf-8" />
-        <title>{{ pagetitle()->get() }}</title>
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta content="width=device-width, initial-scale=1" name="viewport" />
-        <meta content="" name="description" />
-        <meta content="" name="author" />
+@extends( 'front.header' )
 
-        <link href="//fonts.googleapis.com/css?family=Open+Sans:400,300,600,700&subset=all" rel="stylesheet" type="text/css" />
-        <link rel="stylesheet" href="{{ asset( 'css/front/main.css' ) }}">
-        <link rel="stylesheet" href="{{ asset( 'css/front/plugins.css' ) }}">
-        <link rel="stylesheet" href="{{ asset( 'css/front/global.css' ) }}">
-        <link rel="stylesheet" href="{{ asset( 'css/front/layout.css' ) }}">
-
-        <link rel="shortcut icon" href="favicon.ico" />
-    </head>
-    <body class="page-container-bg-solid page-boxed">
-        <div class="page-header">
-            <div class="page-header-top">
-                <div class="container">
-                    <div class="page-logo">
-                        <a class="logo-default navbar-brand uppercase font-dark" href="{{ url( '/' ) }}">{{ settings( 'server_name', 'Perfect World Panel' ) }}</a>
-                    </div>
-                    <a href="#" class="menu-toggler"></a>
-                    <div class="top-menu">
-                        <ul class="nav navbar-nav pull-right">
-                            @if ( Auth::guest() )
-                                <li>
-                                    <a href="{{ url( 'auth/login' ) }}">
-                                       {{ trans( 'main.login_link' ) }}
-                                    </a>
-                                </li>
-                            @else
-                                <li class="hidden-xs">
-                                    <a href="{{ url( 'donate' ) }}">
-                                        {{ trans( 'main.acc_balance', ['money' => Auth::user()->balance(), 'currency' => settings('currency_name')] ) }}
-                                    </a>
-                                </li>
-                                <li class="dropdown dropdown-separator hidden-xs">
-                                    <span class="separator"></span>
-                                </li>
-                                <li class="dropdown dropdown-dark">
-                                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" data-hover="dropdown" data-close-others="true">
-                                        {{ Auth::user()->characterId() ? Auth::user()->characterName() : trans( 'main.select_character' ) }}
-                                    </a>
-                                    <ul class="dropdown-menu dropdown-menu-default">
-                                        @if ( $api->online )
-                                            {{--*/ $roles = Auth::user()->roles() /*--}}
-                                            @if ( count( $roles ) > 0 )
-                                                @foreach( $roles as $role )
-                                                    <li>
-                                                        <a href="{{ url( 'character/select/' . $role['id'] ) }}">
-                                                            {{ $role['name'] }}
-                                                        </a>
-                                                    </li>
-                                                @endforeach
-                                            @else
-                                                <li>
-                                                    <a href="#">
-                                                        {{ trans( 'main.char_list_error' ) }}
-                                                    </a>
-                                                </li>
-                                            @endif
-                                        @else
-                                            <li>
-                                                <a href="#">
-                                                    {{ trans( 'main.server_not_online' ) }}
-                                                </a>
-                                            </li>
-                                        @endif
-                                    </ul>
-                                </li>
-                                <li class="dropdown dropdown-separator">
-                                    <span class="separator"></span>
-                                </li>
-                                <li class="dropdown dropdown-dark">
-                                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" data-hover="dropdown" data-close-others="true">
-                                        @if ( !$agent->isMobile() )
-                                            <img class="avatar" src="{{ Avatar::create( strtoupper( Auth::user()->name ) )->toBase64() }}" />
-                                        @endif
-                                        <span>{{ Auth::user()->name }}</span>
-                                    </a>
-                                    <ul class="dropdown-menu dropdown-menu-default">
-                                        @if ( Auth::user()->isAdmin() )
-                                            <li>
-                                                <a href="{{ url( 'admin' ) }}" target="_blank">
-                                                    <i class="icon-rocket"></i> {{ trans( 'main.acp_link' ) }}
-                                                </a>
-                                            </li>
-                                        @endif
-                                        <li>
-                                            <a href="{{ url( 'account/settings' ) }}">
-                                                <i class="icon-user"></i> {{ trans( 'main.acc_settings' ) }}
-                                            </a>
-                                        </li>
-                                        <li class="divider"> </li>
-                                        <li>
-                                            <a href="{{ url( 'auth/logout' ) }}">
-                                                <i class="icon-key"></i> {{ trans( 'main.logout' ) }}
-                                            </a>
-                                        </li>
-                                    </ul>
-                                </li>
-                            @endif
-                        </ul>
-                    </div>
-                </div>
-            </div>
-            <div class="page-header-menu">
-                <div class="container">
-                    <div class="hor-menu">
-                        <ul class="nav navbar-nav">
-                            @foreach( $apps as $app )
-                                @if ( $app->enabled )
-                                    <li {{ ( $app->key == 'news' ) ? ( Request::is( '/' ) ? 'class=active' : NULL ) : ( Request::is( $app->key . '*' ) ? 'class=active' : NULL ) }}>
-                                        <a href="{{ ( $app->key == 'news' ) ? url( '/') : url( '/' . $app->key ) }}"> {{ trans( 'main.apps.' . $app->key ) }} </a>
-                                    </li>
-                                @endif
-                            @endforeach
-                            <li class="menu-dropdown classic-menu-dropdown visible-lg" style="position: absolute; right: 0">
-                                <a href="#">
-                                    <i class="icon-globe"></i>
-                                    <span class="arrow"></span>
-                                </a>
-                                <ul class="dropdown-menu pull-right">
-                                    @foreach( $languages as $language )
-                                        <li>
-                                            <a href="{{ Request::url() . '?language=' . $language }}">
-                                                <img src="{{ asset( 'img/flags/' . $language . '.png' ) }}"> {{ trans( 'language.' . $language ) }}
-                                            </a>
-                                        </li>
-                                    @endforeach
-                                </ul>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="page-container">
-            <div class="page-content-wrapper">
-                {!! Breadcrumbs::render() !!}
-                <div class="page-content">
-                    <div class="container">
-                        <div class="page-content-inner">
-                            @include( 'errors.list' )
-                            @include( 'flash::message' )
-                            <div class="row">
-                                <div class="col-md-{{ Request::is( 'shop*' ) ? '12' : '9' }}">
-                                    @yield( 'content' )
+@section( 'content' )
+    @include( 'errors.list' )
+    @include( 'front.shop.sidebar' )
+    <div class="col-md-9">
+        <div class="row">
+            @if( $items->count() > 0 )
+                @foreach( $items as $item )
+                    <div class="col-md-6">
+                        <form action="{{ url( 'shop/purchase/' . $item->id ) }}" method="post">
+                            {!! csrf_field() !!}
+                            <div class="grid-item box bg-white">
+                                <div class="grid_icon">
+                                    <img src="{{ asset( File::exists( base_path( 'public/img/icons/' . $item->item_id . '.gif' ) ) ? 'img/icons/' . $item->item_id . '.gif' : 'img/icons/0.gif' ) }}" alt="{{ $item->name }}">
                                 </div>
-                                @unless( Request::is( 'shop*' ) )
-                                    <div class="col-md-3">
-                                        @include( 'front.widgets' )
+                                <div class="grid-item-title">
+                                    <span class="font-dark">{{ $item->name }}</span>
+                                </div>
+                                <div class="grid-item-price font-blue">
+                                    @if ( $item->discount)
+                                        <s>{{ $item->price }}</s> <span class="font-red">{{ $item->price - ( ( $item->price / 100 ) * $item->discount ) }}</span> {{ settings( 'currency_name' ) }}
+                                    @else
+                                        {{ $item->price . ' ' . settings( 'currency_name' ) }}
+                                    @endif
+                                </div>
+                                <div class="caption mt-xs p-sm">
+                                    <div class="scroller" style="height:100px" data-always-visible="1" data-rail-visible="1" data-rail-color="#111" data-handle-color="#000">
+                                        {!! $item->description !!}
                                     </div>
-                                @endunless
+                                    <div class="grid-item-buy text-center mt-md">
+                                        @if ( $item->shareable )
+                                            <button class="btn purple gift-button" data-toggle="modal" data-target="#giftModal_{{ $item->id }}"><i class="icon-paper-plane"></i> {{ trans( 'shop.gift' ) }} </button>
+                                        @endif
+                                        <button class="btn green">{{ trans( 'shop.buy' ) }}</button>
+                                    </div>
+                                </div>
                             </div>
+                        </form>
+                    </div>
+                    <!-- Modal -->
+                    <div class="modal fade" id="giftModal_{{ $item->id }}" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+                        <div class="modal-dialog" role="document">
+                            <form method="post" action="{{ url( 'shop/gift/' . $item->id ) }}" data-abide>
+                                {!! csrf_field() !!}
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                                        <h4 class="modal-title" id="myModalLabel">{{ trans( 'shop.gift_title', ['name' => $item->name]) }}</h4>
+                                    </div>
+                                    <div class="modal-body">
+                                        @if ( !Auth::user()->characterId() )
+                                            <p class="text-center font-red"> {{ trans( 'shop.select_char_first' ) }}</p>
+                                        @else
+                                            {{--*/ $friends = $api->getRoleFriends( Auth::user()->characterId() ) /*--}}
+                                            @if ( !$friends )
+                                                <div class="text-center">
+                                                    {{ trans( 'shop.no_friends') }}
+                                                </div>
+                                            @else
+                                                <div class="text-info text-center">
+                                                    <small>{{ trans( 'shop.recently_added_friends') }}</small>
+                                                </div>
+                                                <div class="row">
+                                                    @foreach( $friends as $friend )
+                                                        @foreach ( $friend as $key => $value )
+                                                            <div class="col-md-3">
+                                                                <input id="{{ $value['rid'] }}" value="{{ $value['rid'] }}" name="friends[]" type="checkbox"/>
+                                                                <label for="{{ $value['rid'] }}">{{ $value['name'] }}</label>
+                                                            </div>
+                                                        @endforeach
+                                                    @endforeach
+                                                </div>
+                                            @endif
+                                        @endif
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button class="btn btn-primary " name="submit" type="submit">{{ trans( 'shop.send_gift' ) }}</button>
+                                    </div>
+                                </div>
+                            </form>
                         </div>
                     </div>
+                @endforeach
+            @else
+                <div class="portlet light">
+                    <h4 class="portlet-body text-center mb-sm p-none">
+                        {{ trans( 'main.no_results') }}
+                    </h4>
                 </div>
-            </div>
+            @endif
         </div>
-        @include( 'front.footer' )
+        <div class="row text-center">
+            {!! $items->render() !!}
+        </div>
+    </div>
+@endsection
 
-        <script src="{{ asset( 'js/front/main.js' ) }}"></script>
-        <script src="{{ asset( 'js/front/plugins.js' ) }}"></script>
-        <script src="{{ asset( 'js/front/global.js' ) }}"></script>
-        <script src="{{ asset( 'js/front/layout.js' ) }}"></script>
-
-        @yield( 'footer' )
-    </body>
-</html>
+@section( 'footer' )
+    @parent
+    <script>
+        $(".gift-button").click(function(e){
+            e.preventDefault(); // cancel default behavior
+        });
+    </script>
+<script type="text/javascript">
+        $(function(){
+            $('.green').click(function() {
+                $(this).hide();
+            });
+        });
+    </script>
+@endsection
